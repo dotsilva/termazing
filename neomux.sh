9e45a62616d225394ff339b10de12934860d9b98
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-19 19:49:17 -0300

    Update neomux.sh

--- neomux.sh
+++ neomux.sh
@@ -2,11 +2,13 @@
 
 pkg update && pkg upgrade -y &&
 pkg install mandoc &&
-pkg install which mlocate nodejs-lts lua51* luarocks stylua python ruby dart cmake ninja clang rust ripgrep fzf fd curl ncurses-utils git lazygit neovim fastfetch
+pkg install which mlocate nodejs-lts lua51* wget luarocks stylua python ruby dart cmake ninja clang rust ripgrep fzf fd curl ncurses-utils git lazygit neovim fastfetch
 gem update
 gem install neovim
 pip install pynvim
 cargo install ast-grep
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.profile
+source ~/.profile
 curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
 getnf
 # required
@@ -20,4 +22,6 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim &&
 rm -rf ~/.config/nvim/.git &&
 echo "HORA DO SHOW, LAZYVRUUUUUUUUUUUUM..."
 sleep 3
+command -v ast-grep
+ast-grep --version
 nvim
