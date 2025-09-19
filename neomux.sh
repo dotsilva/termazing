59160431b4003d9d07fd6ac2ee948b08e33ffd71
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-19 20:26:26 -0300

    Update neomux.sh

--- neomux.sh
+++ neomux.sh
@@ -2,7 +2,7 @@
 
 pkg update && pkg upgrade -y &&
 pkg install mandoc &&
-pkg install which mlocate nodejs-lts lua51* wget luarocks stylua python ruby dart cmake ninja clang rust ripgrep fzf fd curl ncurses-utils git lazygit neovim fastfetch
+pkg install which mlocate nodejs-lts lua51* lua-language-server wget luarocks stylua python ruby dart cmake ninja clang rust ripgrep fzf fd curl ncurses-utils git lazygit neovim fastfetch
 gem update
 gem install neovim
 pip install pynvim
