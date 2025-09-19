6076bcb5a6355cd5d36b6c16af00bcf692033128
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-19 19:34:39 -0300

    Update neomux.sh

--- neomux.sh
+++ neomux.sh
@@ -9,3 +9,15 @@ pip install pynvim
 cargo install ast-grep
 curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
 getnf
+# required
+mv ~/.config/nvim{,.bak} &&
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim &&
+rm -rf ~/.config/nvim/.git &&
+echo "HORA DO SHOW, LAZYVRUUUUUUUUUUUUM..."
+sleep 3
+nvim
