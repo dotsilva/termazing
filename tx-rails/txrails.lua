731dc0fb59b5e6d2d7a04dfbaf8ba3bd89edd703
dotsilva <devbrunodasilva@gmail.com>
2025-09-21 16:08:49 -0300

    hm

--- tx-rails/txrails.lua
+++ tx-rails/txrails.lua
@@ -1,22 +1,23 @@
--- File: ~/.config/nvim/lua/plugins/rails-complements.lua
--- Description: Complementary plugins for Ruby on Rails development.
+-- File: ~/.config/nvim/lua/plugins/rails.lua
+-- Description: Plugins essenciais para desenvolvimento com Ruby on Rails.
 
 return {
-  -- Bundler command integration (:Bundle open, :Bundle install)
+  -- O plugin ESSENCIAL para desenvolvimento Rails.
+  -- Inclui funcionalidades de rake, bundler, navegação entre arquivos MVC,
+  -- comandos do console rails, etc. Substitui vim-rake e vim-bundler.
   {
-    "tpope/vim-bundler",
+    "tpope/vim-rails",
     lazy = true,
     ft = { "ruby" },
+    -- Opcional: Adicionar dependências se precisar garantir a ordem de carregamento,
+    -- mas para o vim-rails geralmente não é necessário.
+    -- dependencies = {
+    --   "nvim-lua/plenary.nvim",
+    -- },
   },
 
-  -- Rake task integration (:Rake db:migrate)
-  {
-    "tpope/vim-rake",
-    lazy = true,
-    ft = { "ruby" },
-  },
-
-  -- Slim template file support
+  -- Suporte para a linguagem de template Slim.
+  -- Mantenha este plugin apenas se você usa arquivos .slim em seus projetos.
   {
     "slim-template/vim-slim",
     lazy = true,
