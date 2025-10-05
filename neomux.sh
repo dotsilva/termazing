bd7c1c9255b23158cfcf1b0db4b3c792ff83a655
dotsilva <devbrunodasilva@gmail.com>
2025-10-05 07:02:23 -0300

    improving readability by using caps lock for all section titles

--- neomux.sh
+++ neomux.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
 echo "  ----------------------------  "
-echo ">> installing pkg dependencies <<"
+echo ">> PKG UPGRADE && INSTALL <<"
 echo "  ----------------------------  "
 pkg update
 yes | pkg upgrade &&
-yes | pkg install build-essential termux-api which \
+  yes | pkg install build-essential termux-api which \
     mlocate nodejs-lts openssl \
     lua51 luajit lua-language-server \
     libxml2 libxslt htop \
@@ -16,7 +16,7 @@ yes | pkg install build-essential termux-api which \
     fastfetch shfmt shellcheck binutils neovim
 
 echo "----------------------------"
-echo ">> applying .files, font and theme <<"
+echo ">> NERD FONTS SCRIPT AND DOTFILES <<"
 echo "----------------------------"
 curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
 
@@ -34,13 +34,12 @@ sleep 3
 fastfetch
 
 echo "----------------------------"
-echo ">> confirming clipboard <<"
+echo ">> CONFIRMING CLIPBOARD <<"
 echo "----------------------------"
 command -v termux-clipboard-set
 
 echo "----------------------------"
-echo ">> ruby lsp, dap and etc <<"
-echo ">> building nokogiri gem <<"
+echo ">> NOKOGIRI GEM AND GEM INSTALL<<"
 echo "----------------------------"
 gem install nokogiri -- --use-system-libraries &&
   gem install rails bundler neovim erb-formatter \
@@ -48,24 +47,24 @@ gem install nokogiri -- --use-system-libraries &&
     haml_lint
 
 echo "----------------------------"
-echo ">> lua luacheck <<"
+echo ">> LUAROCKS INSTALL LUACHECK <<"
 echo "----------------------------"
 luarocks install luacheck
 
 echo "----------------------------"
-echo ">> installing npm dependencies <<"
+echo ">> NPM -G INSTALL <<"
 echo "----------------------------"
 npm install -g typescript-language-server \
   @fsouza/prettierd bash-language-server \
   neovim typescript eslint_d
 
 echo "----------------------------"
-echo ">> compiling cargo dependencies <<"
+echo ">> CARGO INSTALL AST-GREP <<"
 echo "----------------------------"
 cargo install ast-grep
 
 echo "----------------------------"
-echo ">> installing lazyvim <<"
+echo ">> INSTALLING LAZYVIM <<"
 echo "----------------------------"
 # required
 mv ~/.config/nvim{,.bak} &&
@@ -77,17 +76,16 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim &&
   rm -rf ~/.config/nvim/.git
 
 echo "----------------------------"
-echo ">> moving .lua files <<"
+echo ">> MOVING DOTLUA FILES <<"
 echo "----------------------------"
 cp -r ~/termazing/tx-rails/*.lua ~/.config/nvim/lua/plugins/
 cp -r ~/termazing/thm-ashen.lua ~/.config/nvim/lua/plugins/
 
 echo "----------------------------"
-echo ">> lazyvim first load (headless) <<"
+echo ">> STARTING LAZY FIRST TIME - HEADLESS <<"
 echo "----------------------------"
 nvim --headless "+Lazy! sync" +qa
 
 echo "----------------------------"
-echo ">> USE getnf to install nerd fonts <<"
+echo ">> DONT FORGET NERD FONTS - getnf <<"
 echo "----------------------------"
-source ~/.profile
